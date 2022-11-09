8607ce5fc77b3952927d52eabcae9a7ef8fd6524
Mint_Caramel <91499156+LizaNilova@users.noreply.github.com>
2022-11-10 01:28:36 +0200

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,22 +4,20 @@ const UserSchema = new mongoose.Schema({
   full_name: { 
     type: String, 
     required: true, 
-    default: "" 
   },
   email: { 
     type: String, 
-    required: true, 
-    default: "" 
+    required: true,
+    unique: true,
   },
   password: { 
     type: String, 
     required: true, 
-    default: "" 
   },
   verified: { 
     type: Boolean, 
     required: true, 
-    default: "false" 
+    default: false
   },
 });
 export default mongoose.model("User", UserSchema);
