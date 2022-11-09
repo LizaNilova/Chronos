9d59e0c4a6815abb11fa82d137f20a49ed00692f
Mint_Caramel <91499156+LizaNilova@users.noreply.github.com>
2022-11-10 01:22:54 +0200

    Code Style

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,26 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-  full_name: { type: String, required: true, default: "" },
-  email: { type: String, required: true, default: "" },
-  password: { type: String, required: true, default: "" },
-  verified: { type: Boolean, required: true, default: "false" },
+  full_name: { 
+    type: String, 
+    required: true, 
+    default: "" 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    default: "" 
+  },
+  password: { 
+    type: String, 
+    required: true, 
+    default: "" 
+  },
+  verified: { 
+    type: Boolean, 
+    required: true, 
+    default: "false" 
+  },
 });
 export default mongoose.model("User", UserSchema);
 
