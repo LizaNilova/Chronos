a5881cd99f940cb99022b42a7b2c221349921b77
Mint_Caramel <91499156+LizaNilova@users.noreply.github.com>
2022-11-10 00:47:58 +0200

    Some code style corrects

--- server/models/Calendar.js
+++ server/models/Calendar.js
@@ -1,19 +1,49 @@
 import mongoose from "mongoose";
 
-const CalendarSchema = new mongoose.Schema({
-  name: { type: String, required: true, default: "New calendar" },
-  description: { type: String, default: "" },
-  type: { type: String, required: true, default: "additionally" },
-  color_event: { type: String, required: true, default: "fffff" },
-  icon: { type: String, required: true, default: "" },
-  visible: { type: Boolean, required: true, default: "true" },
-  national_holidays: { type: Boolean, required: true, default: "true" },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+const CalendarSchema = new mongoose.Schema(
+  {
+    name: { 
+      type: String, 
+      required: true, 
+      default: "New calendar" 
+    },
+    description: { 
+      type: String, 
+      default: "" 
+    },
+    type: { 
+      type: String, 
+      required: true, 
+      default: "additionally" 
+    },
+    color_event: { 
+      type: String, 
+      required: true, 
+      default: "fffff" 
+    },
+    icon: { 
+      type: String, 
+      required: true, 
+      default: "" 
+    },
+    visible: { 
+      type: Boolean, 
+      required: true, 
+      default: "true" 
+    },
+    national_holidays: { 
+      type: Boolean, 
+      required: true, 
+      default: "true" 
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    members: [{ 
+      type: mongoose.Schema.Types.ObjectId, ref: "User" 
+    }],
 });
 export default mongoose.model("Calendar", CalendarSchema);
 
