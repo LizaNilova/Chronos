6c5de051ccc3f1d7b881639b806770c1214f93a5
Mint_Caramel <91499156+LizaNilova@users.noreply.github.com>
2022-11-10 01:34:34 +0200

    Update Event.js

--- server/models/Event.js
+++ server/models/Event.js
@@ -1,14 +1,32 @@
 import mongoose from "mongoose";
 
 const EventSchema = new mongoose.Schema({
-  name: { type: String, required: true, default: "New event" },
-  description: { type: String },
-  date_start: { type: Date, required: true },
-  date_end: { type: Date, required: true },
-  calendars: [
-    { type: mongoose.Schema.Types.ObjectId, ref: "Calendar", required: true },
-  ],
-  type: { type: String, required: true, default: "task" },
+  name: { 
+    type: String, 
+    required: true, 
+    default: "New event" 
+  },
+  description: { 
+    type: String 
+  },
+  date_start: { 
+    type: Date, 
+    required: true 
+  },
+  date_end: { 
+    type: Date, 
+    required: true 
+  },
+  calendars: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Calendar", 
+    required: true 
+  }],
+  type: { 
+    type: String, 
+    required: true, 
+    default: "task" 
+  },
 });
 export default mongoose.model("Event", EventSchema);
 
